28271015646bd4e9bd085af0c7e740022c46168e
suvasish114 <misuvasish114@gmail.com>
2022-08-14 17:20:21 +0530

    flash message close button added

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@
 
 from flask import Flask
 from config import config
+from flask_moment import Moment
+
+moment = Moment()
 
 def create_app(config_name):
     ''' Creates an application instances and return it to the caller
@@ -11,6 +14,8 @@ def create_app(config_name):
     app = Flask(__name__)                               # application instance obeject
     app.config.from_object(config[config_name])         # configure with defined config-type
     config[config_name].init_app(app)                   # initialization application instance
+
+    moment.init_app(app)                                # register flask_moment
 
     from .auth import authentication_blueprint          # registering blurprints to the instance
     from .general import general_blueprint
